0e74ea269c079253c2793a807e6381563a49aadc
KJK <KJK@KJK.local>
2018-08-17 10:38:44 -0600

    First commit

---
+++ crud-client/src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => (
+  <div class="jumbotron">
+    <h1 className="display-3">Kyles Store</h1>
+    <p className="lead">Buy Some Stuff!</p>
+    <hr className="my-4"></hr>
+    <p>Here you can Create, Read, Update and Delete Products! </p>
+    <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
+)
+
+export default LandingPage;
