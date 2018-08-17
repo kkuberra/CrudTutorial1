9c682eb7f471c5b3801b9f09d60def260d7300e3
KJK <KJK@KJK.local>
2018-08-17 11:26:54 -0600

    added first route

--- crud-client/src/components/LandingPage.js
+++ crud-client/src/components/LandingPage.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const LandingPage = () => (
   <div class="jumbotron">
-    <h1 className="display-3">Kyles Store</h1>
+    <h1 className="display-3">Kyle's Store</h1>
     <p className="lead">Buy Some Stuff!</p>
     <hr className="my-4"></hr>
     <p>Here you can Create, Read, Update and Delete Products! </p>
     <p className="lead">
-      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      <a className="btn btn-primary btn-lg" href="#" role="button">See the Goods</a>
     </p>
   </div>
 )
---
+++ crud-client/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+<div class="alert alert-danger">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+</div>
+)
+
+export default NotFound;
+ 
